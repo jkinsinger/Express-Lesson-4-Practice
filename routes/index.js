e762320b7e231337f02a61026d5c840cdba565a0
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-11 06:44:31 -0600

    added actor GET & POST routes & confirmed all working

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -43,4 +44,48 @@ router.get('/actor/:id', function(req, res, next) {
     console.log(result)
   })
 });
+
+//get actor method
+
+const actorList = `SELECT * from actor`;
+
+router.get('/actor', function(req, res){
+  connection.query(actorList, function(err, result){
+    res.render('actor', {
+      actors: result
+    });
+  });
+});
+
+router.post('/actor', function(req, res, next){
+  console.log(req .body);
+  const newActor = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+  };
+
+  const selectActor = `SELECT *
+    FROM actor
+    WHERE first_name = '${newActor.first_name}'
+    AND last_name = '${newActor.last_name}'`;
+
+
+  connection.query(selectActor, function(err, result){
+    if (result.length > 0) {
+      res.send('Sorry, that actor already exists') ;
+    } else {
+      let newActorQuery = `INSERT INTO actor(first_name, last_name)
+        VALUES('${newActor.first_name}', '${newActor.last_name}');`
+
+      connection.query(newActorQuery, function(err, insertResult){
+        if(err) {
+          res.send('error', {message: 'Oops, something went wrong'});
+          // res.send('Oops, something went wrong');
+        } else {
+          res.redirect('/actor');
+        }
+      });
+    }
+  });
+  });
 module.exports = router;
